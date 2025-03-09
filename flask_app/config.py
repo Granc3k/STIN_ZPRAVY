86f4264d7dd926937030faa75e023f7960817701
Martin Granc3k Šimon - PRDOLA <mart.simon17@gmail.com>
2025-03-09 23:13:33 +0100

    feat: tests for current application 93%

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,4 +1,5 @@
 import os
+from flask_app import config_keys
 
 # Preferujeme načítání API klíče z prostředí (produkce, GitHub Actions)
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
@@ -14,3 +15,5 @@ if not NEWS_API_KEY:
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENGINE_OPTIONS = {"connect_args": {"check_same_thread": False}}
+NEWS_API_KEY = config_keys.NEWS_API_KEY
+# NEWS_API_KEY = secrets.NEWS_API_KEY
