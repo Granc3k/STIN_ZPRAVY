2c937930b8fc2ad9d7f996e6b77a75b81930d5d0
Martin Šimon <mart.simon17@gmail.com>
2025-03-14 07:45:27 +0100

    Update config.py

--- flask_app/config.py
+++ flask_app/config.py
@@ -4,5 +4,5 @@ from flask_app import config_keys
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENGINE_OPTIONS = {"connect_args": {"check_same_thread": False}}
-NEWS_API_KEY = config_keys.NEWS_API_KEY
-# NEWS_API_KEY = secrets.NEWS_API_KEY
+# NEWS_API_KEY = config_keys.NEWS_API_KEY
+NEWS_API_KEY = secrets.NEWS_API_KEY
