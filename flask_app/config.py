8f54494702b2c686213929ca2870000ea656b142
Martin Granc3k Simon - BOBEK <mart.simon17@gmail.com>
2025-03-14 13:20:22 +0100

    updated config

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,5 +1,4 @@
 import os
-from flask_app import config_keys
 
 LIST_SIZE = 5  # Počet zpráv na stránku
 
@@ -29,5 +28,3 @@ if not NEWS_API_KEY or not OPEN_AI_API_KEY:
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENGINE_OPTIONS = {"connect_args": {"check_same_thread": False}}
-NEWS_API_KEY = config_keys.NEWS_API_KEY
-# NEWS_API_KEY = secrets.NEWS_API_KEY
